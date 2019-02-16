4e9e79758c1b84c200fb3ffc24ea161797d08b1d
Greg <gregoryamitten@gmail.com>
2019-02-16 14:22:59 +0000

    Migrated to jodatime

--- app/services/generator/eventgenerator/Event.scala
+++ app/services/generator/eventgenerator/Event.scala
@@ -1,12 +1,13 @@
 package services.generator.eventgenerator
 
+import org.joda.time.LocalTime
 import services.generator.eventgenerator.EventType.EventType
 
 object Event {
-  def apply(name: String, eventType: EventType, occurrencesPerWeek: Int): Event = new Event(name, eventType, occurrencesPerWeek)
+  def apply(name: String, eventType: EventType, occurrencesPerWeek: LocalTime): Event = new Event(name, eventType, occurrencesPerWeek)
 }
 
-class Event(val name:String, val eventType:EventType, val hours: Int) {
+class Event(val name:String, val eventType:EventType, val hours: LocalTime) {
   override def toString: String = s"$name - $eventType - $hours"
 }
 
--- app/services/generator/eventgenerator/EventGenerator.scala
+++ app/services/generator/eventgenerator/EventGenerator.scala
@@ -1,9 +1,10 @@
 package services.generator.eventgenerator
 
+import org.joda.time.LocalTime
 import services.generator.Generator
 
 import scala.util.Random
 
 object EventGenerator extends Generator[Event] {
-  override def generate(): Event = Event(SubjectGenerator.generate(), EventType.values.toList(Random.nextInt(EventType.values.size - 1)), Random.nextInt(4) + 1)
+  override def generate(): Event = Event(SubjectGenerator.generate(), EventType.values.toList(Random.nextInt(EventType.values.size - 1)), new LocalTime(Random.nextInt(24), Random.nextInt(60)))
 }
