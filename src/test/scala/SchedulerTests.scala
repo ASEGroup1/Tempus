b42c55f33c1fbccf96f7a2f5129860a724f9c8d0
Greg <gregoryamitten@gmail.com>
2019-02-23 17:42:53 +0000

    Fixed unit test

--- src/test/scala/SchedulerTests.scala
+++ src/test/scala/SchedulerTests.scala
@@ -1,7 +1,6 @@
 import junit.framework.TestCase
 import org.joda.time.{DateTime, LocalTime}
 import org.junit.Assert._
-import org.junit.Test
 import services.generator.eventgenerator.EventGenerator
 import services.generator.roomgenerator.RoomGenerator
 import services.scheduler.Scheduler
@@ -13,8 +12,8 @@ class SchedulerTests extends TestCase {
       RoomGenerator.generate(roomCount).map(new Room(_)),
       EventGenerator.generate(eventCount),
       Array(
-        new Period(DateTime.parse("01-01-19"), new Duration(new LocalTime(8, 0), new LocalTime(20, 0))),
-        new Period(DateTime.parse("02-01-19"), new Duration(new LocalTime(8, 0), new LocalTime(20, 0)))
+        new Period(DateTime.parse("2019-01-01"), new Duration(new LocalTime(8, 0), new LocalTime(20, 0))),
+        new Period(DateTime.parse("2019-01-02"), new Duration(new LocalTime(8, 0), new LocalTime(20, 0)))
       ))
   }
 
@@ -25,12 +24,24 @@ class SchedulerTests extends TestCase {
   def testIfEventsDoNotIntersect = {
     var currentEnd = -1
     var currentDay = -1
+    var currentRoom = - 1
 
-    generateSchedule(100, 10).get.groupBy(_.room.id).foreach(s => {
+    generateSchedule(100, 10).get.groupBy(sc => (sc.room.id, sc.day.calendar.dayOfMonth())).foreach(s => {
       s._2.sortBy(_.time.start.getHourOfDay).foreach(e => {
-        print("[Day:" + currentDay + ", Room: " + e.room.id + ", end time:" + currentEnd + "] < [Day: " + e.day.calendar.getDayOfMonth + ", Room: " + e.room.id + ", Start time: " + e.time.start.getHourOfDay + "] < ")
-        if (currentDay != e.day.calendar.getDayOfMonth) currentDay = e.day.calendar.getDayOfMonth
-        if (currentDay < e.day.calendar.getDayOfMonth) currentEnd = -1
+        print("[Day:" + currentDay + ", Room: " + e.room.id + ", end time:" + currentEnd + "] < " +
+          "[Day: " + e.day.calendar.getDayOfMonth + ", Room: " + e.room.id + ", Start time: " + e.time.start.getHourOfDay + "] < ")
+
+        //When room changes reset currentEnd
+        if(currentRoom != e.room.id) {
+          currentRoom = e.room.id
+          currentEnd = -1
+        }
+
+        //When day changes reset currentEnd
+        if (currentDay != e.day.calendar.getDayOfMonth) {
+          currentDay = e.day.calendar.getDayOfMonth
+          currentEnd = -1
+        }
 
         if (currentEnd > e.time.start.getHourOfDay) fail(currentEnd + " > " + e.time.start.getHourOfDay)
         currentEnd = e.time.end.getHourOfDay
