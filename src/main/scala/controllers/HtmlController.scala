d3ca0cd0f8adad516ff22ed6654bf6e5148e5fa7
Rikkey <rikkeypaal@gmail.com>
2019-02-25 21:45:46 +0000

    Setup error handling and changed web file structure

--- src/main/scala/controllers/HtmlController.scala
+++ src/main/scala/controllers/HtmlController.scala
@@ -1,13 +1,29 @@
 package controllers
 
+import java.io.FileNotFoundException
+
 import javax.inject.Inject
 import play.api.mvc._
+import views.ErrorPage
 
 import scala.io.Source
 
 class HtmlController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def display(path: String, file: String) = Action {
-    Ok(Source.fromFile(path + "/" + file).mkString).as("text/html")
+    try {
+      Ok(readFile(path, file)).as("text/html")
+    } catch {
+      case _: FileNotFoundException => NotFound(ErrorPage.notFound(file))
+      case e: Exception => BadRequest(ErrorPage.badRequest(e))
+    }
+  }
+
+  def display(path: String, fileName: String, ext: String): Action[AnyContent] =
+    display(path, fileName + "." + ext)
+
+
+  private def readFile(path: String, file: String): String = {
+    Source.fromFile(path + "/" + file).mkString
   }
 }
