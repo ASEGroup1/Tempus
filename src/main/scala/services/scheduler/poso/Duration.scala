0494e71e2c84f62746fd6059e83fb995b0ba60a3
Greg <GregoryAMitten@gmail.com>
2019-02-27 20:18:47 +0000

    T10 mock schedule api pathway (#30)
    * Implemented JSON conversion and api pathway

--- src/main/scala/services/scheduler/poso/Duration.scala
+++ src/main/scala/services/scheduler/poso/Duration.scala
@@ -3,9 +3,8 @@ package services.scheduler.poso
 import org.joda.time.LocalTime
 
 class Duration(val start: LocalTime, val end: LocalTime) {
-  def duration(): Int = {
-    end.getMillisOfDay - start.getMillisOfDay
-  }
+  def duration(): Int = end.getMillisOfDay - start.getMillisOfDay
 
-  def length = end.getHourOfDay - start.getHourOfDay
+  def this(startHour: Int, endHour:Int) =
+    this(new LocalTime(startHour, 0), new LocalTime(endHour, 0))
 }
