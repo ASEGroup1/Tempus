e4bce4ad6e3195648059fb20234dc8e10baee5cf
richousrick <richousrick@users.noreply.github.com>
2019-02-27 19:42:35 +0000

    23 Setup Frontend (#29)
    * Implemented dynamic routing of static files and ReactJS

---
+++ src/main/scala/controllers/StaticController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import java.io.FileNotFoundException
+
+import javax.inject.Inject
+import play.api.mvc._
+import views.ErrorPage
+
+import scala.io.Source
+
+class StaticController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def display(path: String, file: String, contentType:String) = Action {
+    try {
+      if(contentType.isEmpty)
+        Ok(readFile(path, file))
+      else
+        Ok(readFile(path, file)).as(contentType)
+    } catch {
+      case _: FileNotFoundException => NotFound(ErrorPage.notFound(file)).as("text/html")
+      case e: Exception => BadRequest(ErrorPage.badRequest(e)).as("text/html")
+    }
+  }
+
+  def displayFile(path: String, fileName: String, ext: String, contentType: String): Action[AnyContent] =
+    display(path, fileName + "." + ext, contentType)
+
+  private def readFile(path: String, file: String): String = Source.fromFile(path + "/" + file).mkString
+}
