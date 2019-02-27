934d74e380c10fdfb3f301191a98bb45a2e355a4
Rikkey <rikkeypaal@gmail.com>
2019-02-27 20:14:58 +0000

    Changed error page generation to use exsisting genertor

--- src/main/scala/controllers/ScheduleController.scala
+++ src/main/scala/controllers/ScheduleController.scala
@@ -4,6 +4,7 @@ import javax.inject.{Inject, Singleton}
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
 import services.scheduler.Scheduler
+import views.ErrorPage
 
 @Singleton
 class ScheduleController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
@@ -11,7 +12,7 @@ class ScheduleController @Inject()(cc: ControllerComponents) extends AbstractCon
   def generateScheduleResponse = Action {
     val schedule = Scheduler.generateSchedule(100, 10)
 
-    if(schedule.isEmpty) BadRequest("Could not generate, refresh for new random parameters.").as("text/plain")
+    if(schedule.isEmpty) Ok(ErrorPage.badRequest("Could not generate, refresh for new random parameters.")).as("text/html")
     else Ok (Json.parse(schedule.get.map(_.toJson).mkString("[", ",", "]"))).as("application/json")
   }
 }
