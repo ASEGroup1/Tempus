d2c330390779701920551a772098ebf2400b0ec8
Greg <gregoryamitten@gmail.com>
2019-03-02 18:45:21 +0000

    Sorted time table in appropriate order

--- src/main/scala/controllers/ScheduleController.scala
+++ src/main/scala/controllers/ScheduleController.scala
@@ -13,6 +13,6 @@ class ScheduleController @Inject()(cc: ControllerComponents) extends AbstractCon
     val schedule = Scheduler.generateSchedule(100, 10)
 
     if(schedule.isEmpty) BadRequest(ErrorPage.badRequest("Could not generate, refresh for new random parameters.")).as("text/html")
-    else Ok (Json.parse(schedule.get.map(_.toJson).mkString("[", ",", "]"))).as("application/json")
+    else Ok (Json.parse(schedule.get.sortBy(e => (e.day.calendar.getDayOfYear, e.room.name, e.time.start.getMillisOfDay)).map(_.toJson).mkString("[", ",", "]"))).as("application/json")
   }
 }
