97ba4e03f27ecb43013300b8fe24d5ee4ad2cce4
Greg <gregoryamitten@gmail.com>
2019-03-17 13:36:39 +0000

    Added tests

--- src/test/scala/StudentTests.scala
+++ src/test/scala/StudentTests.scala
@@ -1,10 +1,7 @@
 import entities.people.Student
 import junit.framework.TestCase
+import org.junit.Assert.assertEquals
 
 class StudentTests extends TestCase{
-  def testIfModuleChoices(): Unit = {
-    val x = Student.generate(100)
-
-    print()
-  }
+  def testIfModuleChoices(): Unit = assertEquals(100, Student.generate(100).size)
 }
