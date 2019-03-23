2dd8eb14eb28f0adc57eede04ac19c8055265055
Greg <gregoryamitten@gmail.com>
2019-03-23 17:40:34 +0000

    Removed erroneus test

--- src/main/scala/controllers/ScheduleController.scala
+++ src/main/scala/controllers/ScheduleController.scala
@@ -5,17 +5,16 @@ import java.time.temporal.ChronoField
 import javax.inject.{Inject, Singleton}
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
+import services.scheduler.Scheduler
 import views.ErrorPage
 
 @Singleton
 class ScheduleController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def generateScheduleResponse = Action {
-    Ok("here")
+    val schedule = Scheduler.binPackSchedule(5)
 
-//    val schedule = OldScheduler.generateSchedule(100, 10)
-//
-//    if(schedule.isEmpty) BadRequest(ErrorPage.badRequest("Could not generate, refresh for new random parameters.")).as("text/html")
-//    else Ok (Json.parse(schedule.get.sortBy(e => (e.day.calendar.getDayOfYear, e.room.name, e.time.start.get(ChronoField.MILLI_OF_DAY))).map(_.toJson).mkString("[", ",", "]"))).as("application/json")
+    if(schedule.isEmpty) BadRequest(ErrorPage.badRequest("Could not generate, refresh for new random parameters.")).as("text/html")
+    else Ok (Json.parse(schedule.get.sortBy(e => (e.day.calendar.getDayOfYear, e.room.roomName, e.time.start.get(ChronoField.MILLI_OF_DAY))).map(_.toJson).mkString("[", ",", "]"))).as("application/json")
   }
 }
--- src/test/scala/SchedulerTests.scala
+++ src/test/scala/SchedulerTests.scala
@@ -9,33 +9,6 @@ class SchedulerTests extends TestCase {
 
   def testIfNoEventsGenerateEmptySchedule = assertEquals(None,  Scheduler.binPackSchedule(0))
 
-  def testIfEventsDoNotIntersect = {
-    var currentEnd = -1
-    var currentDay = -1
-    var currentRoom = ""
-
-    Scheduler.binPackSchedule(5).get.groupBy(sc => (sc.room.roomName, sc.day.calendar.getDayOfMonth)).foreach(s => {
-		  s._2.sortBy(sc => (sc.time.start.getHour, sc.time.start.getMinute)).foreach(e => {
-			  print(s"[Day: ${e.day.calendar.getDayOfMonth} Room: ${e.room.roomName} Start time: ${e.time.start.getHour} < ")
-
-        //When room changes reset currentEnd
-        if(currentRoom != e.room.roomName) {
-          currentRoom = e.room.roomName
-          currentEnd = -1
-        }
-
-        //When day changes reset currentEnd
-        if (currentDay != e.day.calendar.getDayOfMonth) {
-          currentDay = e.day.calendar.getDayOfMonth
-          currentEnd = -1
-        }
-
-			  if (currentEnd > e.time.start.getHour) fail(currentEnd + " > " + e.time.start.getHour)
-			  currentEnd = e.time.end.getHour
-      })
-    })
-  }
-
   def testIfEventsCannotFitInSchedule: Unit = {
     assertEquals(None,  Scheduler.binPackSchedule(1))
   }
