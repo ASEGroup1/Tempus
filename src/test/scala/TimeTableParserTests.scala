468e8e1c3bc6cca27709de11e6e665068ced4517
Greg <gregoryamitten@gmail.com>
2019-04-02 16:30:54 +0100

    fixed test

--- src/test/scala/TimeTableParserTests.scala
+++ src/test/scala/TimeTableParserTests.scala
@@ -4,7 +4,7 @@ import org.junit.Before
 import services.parser.TimeTableParser
 
 class TimeTableParserTests extends TestCase{
-  def testIfCorrectAmountOfModulesAreParsed = assertEquals(1917, TimeTableParser.modules.size)
+  def testIfCorrectAmountOfModulesAreParsed = assertEquals(1915, TimeTableParser.modules.size)
   def testIfCorrectAmountOfSchoolsAreParsed = assertEquals(29, TimeTableParser.schools.size)
   def testIfAnyModuleNamesAreValid = assert(TimeTableParser.modules.values.exists(_.moduleName.matches("[A-Z0-9\\(\\),]+")))
   def testIfAnySchoolsArePopulated = assert(TimeTableParser.modules.values.exists(_.school != null))
