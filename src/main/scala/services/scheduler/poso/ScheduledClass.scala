407de63e9fe267c5500269e48a0fd9ffdbdfa2c3
Greg <gregoryamitten@gmail.com>
2019-04-09 16:52:37 +0100

    Improved serialisation test

--- src/main/scala/services/scheduler/poso/ScheduledClass.scala
+++ src/main/scala/services/scheduler/poso/ScheduledClass.scala
@@ -20,4 +20,22 @@ class ScheduledClass(val day: Period, val time: TimePeriod, val room: entities.l
         "endMinute" : "${time.end.getMinute}",
         "day" : "${day.calendar.getDayOfMonth}" }"""
 	}
+
+
+	def canEqual(other: Any): Boolean = other.isInstanceOf[ScheduledClass]
+
+	override def equals(other: Any): Boolean = other match {
+		case that: ScheduledClass =>
+			(that canEqual this) &&
+				day == that.day &&
+				time == that.time &&
+				room == that.room &&
+				className == that.className
+		case _ => false
+	}
+
+	override def hashCode(): Int = {
+		val state = Seq(day, time, room, className)
+		state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+	}
 }
--- src/test/scala/SerialisationTests.scala
+++ src/test/scala/SerialisationTests.scala
@@ -1,6 +1,8 @@
-import java.io.{FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
 import junit.framework.TestCase
+import org.junit.Assert.assertEquals
 import services.parser.TimeTableParser
 import services.scheduler.Scheduler
 import services.scheduler.poso.ScheduledClass
@@ -10,19 +12,13 @@ class SerialisationTests extends TestCase {
   val rooms = SussexRoomScraper.roomDataForSession
   val events = TimeTableParser.modules.flatMap(m => m._2.requiredSessions.map(m._1 -> _.durationInHours)).toSet
 
-  def test() = {
-    val sc = Scheduler.binPackSchedule(5, rooms, events).get.head
+  def testIfSerialisedItemsAreTheSameDeserialised() = {
+    val classes = Scheduler.binPackSchedule(5, rooms, events).get
 
-    val oos = new ObjectOutputStream(new FileOutputStream("/tmp/nflx"))
-    oos.writeObject(sc)
-    oos.close
+    val bo = new ByteArrayOutputStream
+    new ObjectOutputStream(bo).writeObject(classes)
+    val serializedObject = bo
 
-    // (3) read the object back in
-    val ois = new ObjectInputStream(new FileInputStream("/tmp/nflx"))
-    val sClass = ois.readObject.asInstanceOf[ScheduledClass]
-    ois.close
-
-    // (4) print the object that was read back in
-    println(sClass)
+    assertEquals(classes.size, new ObjectInputStream(new ByteArrayInputStream(serializedObject.toByteArray)).readObject.asInstanceOf[List[ScheduledClass]].size)
   }
 }
