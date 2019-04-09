2966471546fdef8d2fa510c6fda7c4dd47bc7926
Greg <gregoryamitten@gmail.com>
2019-04-09 15:53:48 +0100

    Leaving mongo for now

--- src/main/scala/Dao.scala
+++ src/main/scala/Dao.scala
@@ -4,24 +4,22 @@ import org.mongodb.scala.bson.collection.mutable.Document
 class Dao {
 
   def test() = {
-    val db = MongoClient("mongodb://ec2-3-9-11-63.eu-west-2.compute.amazonaws.com:27017").getDatabase("master")
+    val client =  MongoClient("mongodb://ec2-3-9-11-63.eu-west-2.compute.amazonaws.com")
+    val db = client.getDatabase("master")
     val collection: MongoCollection[Document] = db.getCollection("test")
 
     val doc: Document = Document("_id" -> 0, "name" -> "MongoDB", "type" -> "database",
       "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
 
     collection.insertOne(doc).subscribe(new Observer[Completed] {
-      override def onNext(result: Completed): Unit =
-        println("Inserted")
+      override def onNext(result: Completed): Unit = println("Inserted")
 
-      override def onError(e: Throwable): Unit =
-        println("Failed")
+      override def onError(e: Throwable): Unit = println("Failed: " + e.getMessage)
 
-      override def onComplete(): Unit =
-        println("Completed")
+      override def onComplete(): Unit = println("Completed")
 
       override def onSubscribe(subscription: Subscription) = {
-        super.onSubscribe(subscription)
+        subscription.request(Long.MaxValue)
         print("subscribed")
       }
     })
