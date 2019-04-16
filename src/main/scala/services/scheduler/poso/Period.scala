1389134b72aa980bafcf15c3eab548fd2f5dcc8b
Greg <GregoryAMitten@gmail.com>
2019-04-16 14:53:58 +0100

    T36 serialise data into key value pairs (#45)
    * Serialised schedule and db framework both setup

--- src/main/scala/services/scheduler/poso/Period.scala
+++ src/main/scala/services/scheduler/poso/Period.scala
@@ -3,4 +3,5 @@ package services.scheduler.poso
 import entities.timing.TimePeriod
 import java.time.OffsetDateTime
 
-class Period(val calendar: OffsetDateTime, val timePeriod: TimePeriod)
+@SerialVersionUID(100L)
+class Period(val calendar: OffsetDateTime, val timePeriod: TimePeriod) extends Serializable
---
+++ src/test/scala/SerialisationTests.scala
@@ -0,0 +1,32 @@
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
+import db.Dao
+import junit.framework.TestCase
+import org.junit.Assert.assertEquals
+import services.parser.TimeTableParser
+import services.scheduler.Scheduler
+import services.scheduler.poso.ScheduledClass
+import services.sussexroomscraper.SussexRoomScraper
+
+class SerialisationTests extends TestCase {
+  val rooms = SussexRoomScraper.roomDataForSession
+  val events = TimeTableParser.modules.flatMap(m => m._2.requiredSessions.map(m._1 -> _.durationInHours)).toSet
+  val dao = new Dao
+
+  def testIfSerialisedItemsAreTheSameDeserialised() = {
+    val classes = Scheduler.binPackSchedule(5, rooms, events).get
+
+    val bo = new ByteArrayOutputStream
+    new ObjectOutputStream(bo).writeObject(classes)
+    val serializedObject = bo
+
+    assertEquals(classes.size, new ObjectInputStream(new ByteArrayInputStream(serializedObject.toByteArray)).readObject.asInstanceOf[List[ScheduledClass]].size)
+  }
+
+  //TODO remove after we decided when we're storing data
+  def testIfSerializedTimeTableIsInserted =  dao.insertTimeTable(Scheduler.binPackSchedule(5, rooms, events).get)
+
+  //TODO remove after we decided when we're storing data
+  def testIfRetrievesDeserializedTimeTable = assert(dao.retrieveTimeTable(0).nonEmpty)
+}
