b5273b30d3a3139f7e9cb8bad24f07674b52d8f6
Greg <gregoryamitten@gmail.com>
2019-04-17 17:15:51 +0100

    Improved recursion efficiency

--- src/main/scala/controllers/InsertController.scala
+++ src/main/scala/controllers/InsertController.scala
@@ -10,20 +10,19 @@ import play.api.mvc.{AbstractController, ControllerComponents}
 
 @Singleton
 class InsertController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val studentFields = classOf[Student].getDeclaredFields.map(_.getName) ++ classOf[Person].getDeclaredFields.map(_.getName)
+  val moduleFields = classOf[Module].getDeclaredFields.map(_.getName)
 
   def insertStudent = Action(parse.json) { request =>
     val body = request.body.asInstanceOf[JsObject].value
-    Dao.insert(Student(
-      (classOf[Student].getDeclaredFields.map(_.getName) ++ classOf[Person].getDeclaredFields.map(_.getName))
-        .map(f => f -> (if (body.contains(f)) body(f) else null)).toMap))
+    Dao.insert(Student(studentFields.map(f => f -> (if (body.contains(f)) body(f) else null)).toMap))
 
     Ok("Inserted Student")
   }
 
   def insertModule = Action(parse.json) { request =>
     val body = request.body.asInstanceOf[JsObject].value
-    Dao.insert(Module(classOf[Module].getDeclaredFields.map(_.getName)
-        .map(f => f -> (if (body.contains(f)) body(f) else null)).toMap))
+    Dao.insert(Module(moduleFields.map(f => f -> (if (body.contains(f)) body(f) else null)).toMap))
 
     Ok("Inserted Module")
   }
