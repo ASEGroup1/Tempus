aacb1accfd2a3cfdcad6f5bd4ebd55e2f322e7ac
Josh <jh559@sussex.ac.uk>
2019-04-20 15:36:24 +0100

    Timetables UI

--- frontend/src/views/Output.js
+++ frontend/src/views/Output.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {Home} from "./Home";
+import {Rooms} from "./timetables/Rooms";
+import {Students} from "./timetables/Students";
+import Route from "react-router-dom/es/Route";
 
 export class Output extends React.Component {
 	render() {
-		return <p>TimeTable Outputs</p>;
+		return (
+			<div>
+				<Navbar bg="dark" variant="dark" sticky="top">
+					<Navbar.Brand href="/outputs">Timetables</Navbar.Brand>
+					<Nav className="mr-auto">
+						<Nav.Link href="/timetables/rooms">Rooms</Nav.Link>
+						<Nav.Link href="/timetables/students">Students</Nav.Link>
+					</Nav>
+				</Navbar>
+				<div>
+					<Route exact path="/" component={Home}/>
+					<Route path="/timetables/rooms" component={Rooms}/>
+					<Route path="/timetables/students" component={Students}/>
+				</div>
+			</div>
+		);
 	}
 }
