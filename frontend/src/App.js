5d98e8cde942dc46e7177935f86e71816ec34463
Greg <gregoryamitten@gmail.com>
2019-05-06 18:55:25 +0100

    Added font awesome icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,11 @@ import {DSL} from "./views/DSL";
 import {Output} from "./views/Output";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faCalendarAlt, faDatabase, faUserClock, faWrench, faUserGraduate, faChalkboardTeacher} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
+library.add(faCalendarAlt, faDatabase, faWrench, faUserClock, faUserGraduate, faChalkboardTeacher);
 
 class App extends Component {
 	render() {
@@ -18,11 +22,11 @@ class App extends Component {
 			<Router>
 				<div className="App">
 					<Navbar bg="dark" variant="dark" sticky="top">
-						<Navbar.Brand href="/">Tempus</Navbar.Brand>
+						<Navbar.Brand href="/"><FontAwesomeIcon icon="user-clock"/> &nbsp; Tempus</Navbar.Brand>
 						<Nav className="mr-auto">
-							<Nav.Link href="/dataManagement">Data Management</Nav.Link>
-							<Nav.Link href="/dsl">DSL/Timetabling Constraints</Nav.Link>
-							<Nav.Link href="/outputs">TimeTable Outputs</Nav.Link>
+							<Nav.Link href="/dataManagement"><FontAwesomeIcon icon="database"/> &nbsp;Data Management</Nav.Link>
+							<Nav.Link href="/dsl"><FontAwesomeIcon icon="wrench"/> &nbsp;Custom Constraints</Nav.Link>
+							<Nav.Link href="/outputs"><FontAwesomeIcon icon="calendar-alt"/> &nbsp;Timetable</Nav.Link>
 						</Nav>
 					</Navbar>
 					<div className="content">
