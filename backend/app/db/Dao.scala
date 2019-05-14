fc524e7ce32ad73fe2935c3c22e248c42ff35072
gregoryamitten@gmail.com <gregoryamitten@gmail.com>
2019-05-14 14:41:14 +0100

    reverted code to fix intellij error

--- backend/app/db/Dao.scala
+++ backend/app/db/Dao.scala
@@ -8,7 +8,7 @@ trait Dao[T] {
   val tableName: String
   val handler: ResultSetHandler[Array[AnyRef]]
   //Even though these are set in environment variables there is currently a bug in intellij that sets env variables as properties instead in play
-  val conn = DriverManager.getConnection(sys.props("DB_URL"), sys.props("DB_USER"), sys.props("DB_PASSWORD"))
+  val conn = DriverManager.getConnection(sys.env("DB_URL"), sys.env("DB_USER"), sys.env("DB_PASSWORD"))
   val run = new QueryRunner()
 
   def insert(data: T)
